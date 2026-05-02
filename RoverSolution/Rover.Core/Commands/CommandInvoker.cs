c4a8487712ea04ee0a96b50f954340f037827ba4
Justin <104700780+CorgiOnNeptune@users.noreply.github.com>
2026-05-01 22:57:57 -0600

    Create SimulationService and CommandInvoker to run an actual simulation on a collection of rovers.

---
+++ RoverSolution/Rover.Core/Commands/CommandInvoker.cs
@@ -0,0 +1,50 @@
+﻿using Rover.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rover.Core.Commands
+{
+    internal static class CommandInvoker
+    {
+
+        /// <summary>
+        /// Create the commands to be executed based on the instructions
+        /// </summary>
+        public static IEnumerable<IRoverCommand> CreateCommands(List<char> instructions)
+        {
+            List<IRoverCommand> commands = new List<IRoverCommand>();
+
+            foreach (char instruction in instructions)
+            {
+                switch (instruction)
+                {
+                    case 'R':
+                        commands.Add(new RotateRightCommand());
+                        break;
+
+                    case 'L':
+                        commands.Add(new RotateLeftCommand());
+                        break;
+
+                    case 'M':
+                        commands.Add(new MoveCommand());
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            return commands;
+        }
+
+        public static void ExecuteAll(IEnumerable<IRoverCommand> commands, MarsRover rover, Plateau plateau)
+        {
+            foreach (IRoverCommand command in commands)
+            {
+                command.Execute(rover, plateau);
+            }
+        }
+    }
+}
---
+++ RoverSolution/Rover.Core/Services/RoverSimulationService.cs
@@ -0,0 +1,38 @@
+﻿using Rover.Core.Commands;
+using Rover.Core.DTOs;
+using Rover.Core.Enums;
+using Rover.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rover.Core.Services
+{
+    public static class RoverSimulationService
+    {
+        public static SimulationResponseDTO RunRoverSimulation(SimulationRequestDTO request)
+        {
+            Plateau plateau = new Plateau(request.PlateauMaxX, request.PlateauMaxY);
+
+            List<string> finalPositions = new List<string>();
+            List<List<Position>> positionHistory = new List<List<Position>>();
+
+            foreach (RoverRequestDTO rover in request.Rovers)
+            {
+                MarsRover newRover = new MarsRover(rover.StartingPosition);
+
+                IEnumerable<IRoverCommand> commands = CommandInvoker.CreateCommands(rover.Commands);
+                CommandInvoker.ExecuteAll(commands, newRover, plateau);
+
+                int finalX = newRover.Position.X;
+                int finalY = newRover.Position.Y;
+                Direction finalDirection = newRover.Position.Direction;
+
+                finalPositions.Add($"{finalX} {finalY} {finalDirection}");
+                positionHistory.Add(newRover.PositionHistory);
+            }
+
+            return new SimulationResponseDTO(finalPositions, positionHistory);
+        }
+    }
+}
