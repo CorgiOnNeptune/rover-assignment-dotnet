e794604cf975aacd56b37e0310bda43602707e50
Justin <104700780+CorgiOnNeptune@users.noreply.github.com>
2026-05-01 22:57:22 -0600

    Create parser for raw string requested format for rover commands.

---
+++ RoverSolution/Rover.Core/DTOs/RoverRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using Rover.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rover.Core.DTOs
+{
+    public class RoverRequestDTO
+    {
+        public Position StartingPosition;
+        internal List<char> Commands;
+
+        public RoverRequestDTO(Position startingPosition, List<char> commands)
+        {
+            StartingPosition = new Position(startingPosition);
+            Commands = commands;
+        }
+    }
+}
---
+++ RoverSolution/Rover.Core/DTOs/SimulationRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rover.Core.DTOs
+{
+    public class SimulationRequestDTO
+    {
+        public int PlateauMaxX;
+        public int PlateauMaxY;
+        public List<RoverRequestDTO> Rovers;
+
+        public SimulationRequestDTO(int plateauMaxX, int plateauMaxY, List<RoverRequestDTO> rovers)
+        {
+            PlateauMaxX = plateauMaxX;
+            PlateauMaxY = plateauMaxY;
+            Rovers = rovers;
+        }
+    }
+}
