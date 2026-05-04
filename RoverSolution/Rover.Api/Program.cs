c97a350012cf0c95d161bb1da199540d84833587
Justin <104700780+CorgiOnNeptune@users.noreply.github.com>
2026-05-04 15:17:20 -0600

    Add Swashbuckle/Swagger

--- RoverSolution/Rover.Api/Program.cs
+++ RoverSolution/Rover.Api/Program.cs
@@ -21,12 +21,17 @@ builder.Services.AddControllers()
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
